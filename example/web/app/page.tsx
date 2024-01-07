ca63037c0e8f63c7ee8bc042312565ddde2d5063
ATLAS2002 <siddharth211102@gmail.com>
2024-01-07 23:15:49 +0530

    Added access hook and the wrapper component

--- example/web/app/page.tsx
+++ example/web/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { useResonate, ResonateWrapper } from "@resonatejs/react";
+
 export default function Page(): JSX.Element {
+  const resonateRef = useResonate();
+
   return (
     <main>
-      <div></div>
+      <ResonateWrapper
+        resonateRef={resonateRef}
+        style={{
+          width: "fit-content",
+          height: "fit-content",
+        }}
+      >
+        <div style={{}}></div>
+      </ResonateWrapper>
     </main>
   );
 }
