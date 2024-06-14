fa3810e58d5b74caf5f1c405e7bd9dc84995dbb1
ATLAS2002 <siddharth211102@gmail.com>
2024-06-14 15:19:49 +0530

    refactor: put dev tools in a shared package

---
+++ configs/eslint/library.js
@@ -0,0 +1,52 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  plugins: ["only-warn"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+  ],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        printWidth: 100,
+        tabWidth: 2,
+        trailingComma: "none",
+        bracketSpacing: true,
+        singleQuote: false,
+        semi: true,
+        plugins: [
+          "prettier-plugin-organize-imports",
+          "prettier-plugin-tailwindcss",
+        ],
+      },
+    ],
+  },
+};
