29dc255f17219025170b366e51237527f5495eb8
ATLAS2002 <siddharth211102@gmail.com>
2024-06-17 17:31:10 +0530

    feat(core): add hook to get the mouse movement data

--- packages/react/src/components/provider.tsx
+++ packages/react/src/components/provider.tsx
@@ -1,34 +1,42 @@
 import React from "react";
 import { defaultValue, ResonateContext, type IContext } from "../hooks";
+import { calculateMagnitude, getCyclicValue } from "../lib/utils";
+import { EventManager } from "../lib/event-manager";
+import type { Vector } from "../types";
 
-enum ActionTypes {
-  MOVE = "MOVE",
+interface PrevState extends Vector {
+  speed: number;
+  time: number;
+}
+
+enum ACTION {
+  SET_POSITION = "POSITION",
+  SET_MOVEMENT = "MOVEMENT",
   ENTER = "ENTER",
   LEAVE = "LEAVE",
 }
 
 type Action =
   | {
-      type: ActionTypes.MOVE;
-      payload: Record<"globalX" | "globalY", number>;
+      type: ACTION;
+      payload: Partial<IContext>;
     }
-  | {
-      type: ActionTypes.ENTER | ActionTypes.LEAVE;
-    };
+  | { type: ACTION.ENTER | ACTION.LEAVE };
 
 const reducer = (state: IContext, action: Action): IContext => {
   switch (action.type) {
-    case ActionTypes.MOVE:
+    case ACTION.SET_POSITION:
+    case ACTION.SET_MOVEMENT:
       return {
         ...state,
         ...action.payload,
       };
-    case ActionTypes.ENTER:
+    case ACTION.ENTER:
       return {
         ...state,
         present: true,
       };
-    case ActionTypes.LEAVE:
+    case ACTION.LEAVE:
       return {
         ...state,
         present: false,
@@ -38,34 +46,79 @@ const reducer = (state: IContext, action: Action): IContext => {
   }
 };
 
-export const ResonateProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [pos, dispatch] = React.useReducer(reducer, defaultValue);
+const RAD_TO_DEG = 180 / Math.PI;
+
+export const ResonateProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
   const ref = React.useRef<HTMLDivElement>(null);
+
+  const [pos, dispatch] = React.useReducer(reducer, defaultValue);
+  const [prev, setPrev] = React.useState<PrevState>({
+    x: 0,
+    y: 0,
+    speed: 0,
+    time: 0,
+  });
+
+  const eventManager = new EventManager();
 
   React.useEffect(() => {
     const elm = ref.current!;
 
-    elm.addEventListener("mousemove", ({ clientX, clientY }) => {
+    const handleCursor = (time: number) => {
+      const dt = time - prev.time;
+      const dx = pos.globalX - prev.x;
+      const dy = pos.globalY - prev.y;
+
+      const speedX = Math.abs(dx) / dt;
+      const speedY = Math.abs(dy) / dt;
+      const speed = calculateMagnitude({ x: dx, y: dy }) / dt;
+
+      const direction = getCyclicValue(Math.atan2(dy, dx) * RAD_TO_DEG, 360);
+      const acceleration = Math.abs(speed - prev.speed) / dt;
+
       dispatch({
-        type: ActionTypes.MOVE,
-        payload: { globalX: clientX, globalY: clientY },
+        type: ACTION.SET_MOVEMENT,
+        payload: {
+          speed,
+          speedX,
+          speedY,
+          direction,
+          acceleration,
+        },
       });
-    });
+      setPrev({
+        x: pos.globalX,
+        y: pos.globalY,
+        speed: pos.speed,
+        time,
+      });
+      requestAnimationFrame(handleCursor);
+    };
+    const handleMouseEnter = () => {
+      dispatch({ type: ACTION.ENTER });
+    };
+    const handleMouseMove = ({ clientX, clientY }: MouseEvent) => {
+      dispatch({
+        type: ACTION.SET_POSITION,
+        payload: {
+          globalX: clientX,
+          globalY: clientY,
+        },
+      });
+    };
+    const handleMouseLeave = () => {
+      dispatch({ type: ACTION.LEAVE });
+    };
 
-    elm.addEventListener("mouseenter", () => {
-      dispatch({ type: ActionTypes.ENTER });
-    });
-
-    elm.addEventListener("mouseleave", () => {
-      dispatch({ type: ActionTypes.LEAVE });
-    });
+    requestAnimationFrame(handleCursor);
+    eventManager.addEvent(elm, "mouseenter", handleMouseEnter);
+    eventManager.addEvent(elm, "mousemove", handleMouseMove);
+    eventManager.addEvent(elm, "mouseleave", handleMouseLeave);
 
     return () => {
-      elm.removeEventListener("mouseenter", () => {});
-      elm.removeEventListener("mousemove", () => {});
-      elm.removeEventListener("mouseleave", () => {});
+      eventManager.executeAll();
     };
   }, [pos]);
 
--- packages/react/src/hooks/context.ts
+++ packages/react/src/hooks/context.ts
@@ -3,12 +3,22 @@ import React from "react";
 export interface IContext {
   globalX: number;
   globalY: number;
+  speedX: number;
+  speedY: number;
+  speed: number;
+  direction: number;
+  acceleration: number;
   present: boolean;
 }
 
 export const defaultValue: IContext = {
   globalX: 0,
   globalY: 0,
+  speedX: 0,
+  speedY: 0,
+  speed: 0,
+  direction: 0,
+  acceleration: 0,
   present: false,
 };
 
