1c92b8a21be268ce251555e6a3892ee391f07f9d
ATLAS2002 <siddharth211102@gmail.com>
2024-06-17 18:31:58 +0530

    feat(docs): create local context in wrapper

--- apps/next/src/components/bg.tsx
+++ apps/next/src/components/bg.tsx
@@ -1,8 +1,40 @@
 "use client";
 
 import { ResonateProvider } from "@resonatejs/react";
-import type { FC, ReactNode } from "react";
+import {
+  useRef,
+  createContext,
+  type FC,
+  type ReactNode,
+  RefObject,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import { Cursor } from "./cursor";
 
-export const Background: FC<{ children: ReactNode }> = ({ children }) => (
-  <ResonateProvider>{children}</ResonateProvider>
-);
+interface IContext {
+  cursorRef: RefObject<HTMLDivElement> | null;
+  showLink: boolean;
+  setShowLink: Dispatch<SetStateAction<boolean>>;
+}
+
+export const Context = createContext<IContext>({
+  cursorRef: null,
+  showLink: false,
+  setShowLink: () => {},
+});
+
+export const Background: FC<{ children: ReactNode }> = ({ children }) => {
+  const cursorRef = useRef<HTMLDivElement>(null);
+  const [showLink, setShowLink] = useState(false);
+
+  return (
+    <ResonateProvider>
+      <Context.Provider value={{ cursorRef, showLink, setShowLink }}>
+        <Cursor ref={cursorRef} />
+        {children}
+      </Context.Provider>
+    </ResonateProvider>
+  );
+};
