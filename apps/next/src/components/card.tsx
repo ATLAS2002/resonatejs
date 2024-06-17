10cac8d0003604046f5d5e8b8fee9166dcff8da0
ATLAS2002 <siddharth211102@gmail.com>
2024-06-17 18:32:28 +0530

    feat(docs): add cursor interactivity in card section

--- apps/next/src/components/card.tsx
+++ apps/next/src/components/card.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { ResonateContainer, tilt, glare, useResonate } from "@resonatejs/react";
+import { useContext } from "react";
+import { Context } from "./bg";
 
 export const Card = ({
   link,
@@ -11,8 +13,18 @@ export const Card = ({
   title: string;
   description: string;
 }) => {
+  const { setShowLink } = useContext(Context);
+
   const trackers = useResonate({
     presets: [glare(), tilt()],
+    customEventListeners: () => ({
+      mouseenter: () => {
+        setShowLink(true);
+      },
+      mouseleave: () => {
+        setShowLink(false);
+      },
+    }),
   });
 
   return (
@@ -22,7 +34,7 @@ export const Card = ({
         className="group rounded-lg px-5 py-4 transition-colors hover:bg-gray-100 hover:dark:bg-neutral-800/30"
       >
         <h2 className="mb-3 text-2xl font-semibold group-hover:scale-110 group-hover:translate-x-2 group-hover:-translate-y-1 transition-all">
-          {`${title} `}
+          {title}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
             -&gt;
           </span>
